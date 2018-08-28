784ea0f378f16ef69419b97cdedee33317435096
achalbi <achal.rvce@gmail.com>
2018-08-28 21:22:02 +0530

    minor deploy fixes

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :smarthood, SmarthoodWeb.Endpoint,
   url: [scheme: "https", host: "smarthood-daily.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
